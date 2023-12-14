fad2a1450dcd5a93d12937996828b66119b99262
localhost-server <99524394+localhost-server@users.noreply.github.com>
2023-12-14 15:07:17 +0530

    Update cnnp.py

--- code/cnnp.py
+++ code/cnnp.py
@@ -1,10 +1,10 @@
-# 论文中原始的CNN网络结构
+# Original CNN network structure from the paper
 
-# construct the proposed CNNP
+# Construct the proposed CNNP
 import torch.nn as nn
 from torch import cat
 
-class CNNP(nn.Module): # CNN预测器
+class CNNP(nn.Module):  # CNN Predictor
     def __init__(self):
         super(CNNP, self).__init__()
         channel = 32
@@ -77,25 +77,25 @@ class CNNP(nn.Module): # CNN预测器
         layers = [nn.Conv2d(channel, 1, 3, 1, 1)]
         self.conv18 = nn.Sequential(*layers)
 
-    def forward(self, images): # 前向
+    def forward(self, images):  # Forward pass
         out1_0 = self.conv1_0(images)
         out1_1 = self.conv1_1(images)
         out1_2 = self.conv1_2(images)
         out2 = out1_0 + out1_1 + out1_2
         out3 = self.conv3(out2)
-        out4 = self.conv4(out3+out2)
-        out5 = self.conv5(out4+out2)
-        out6 = self.conv6(out5+out2)
-        out7 = self.conv7(out6+out2)
-        out8 = self.conv8(out7+out2)
-        out9 = self.conv9(out8+out2)
-        out10 = self.conv10(out9+out2)
-        out11 = self.conv11(out10+out2)
-        out12 = self.conv12(out11+out2)
-        out13 = self.conv13(out12+out2)
-        out14 = self.conv14(out13+out2)
-        out15 = self.conv15(out14+out2)
-        out16 = self.conv16(out15+out2)
-        out17 = self.conv17(out16+out2)
-        out18 = self.conv18(out17+out2)
+        out4 = self.conv4(out3 + out2)
+        out5 = self.conv5(out4 + out2)
+        out6 = self.conv6(out5 + out2)
+        out7 = self.conv7(out6 + out2)
+        out8 = self.conv8(out7 + out2)
+        out9 = self.conv9(out8 + out2)
+        out10 = self.conv10(out9 + out2)
+        out11 = self.conv11(out10 + out2)
+        out12 = self.conv12(out11 + out2)
+        out13 = self.conv13(out12 + out2)
+        out14 = this.conv14(out13 + out2)
+        out15 = self.conv15(out14 + out2)
+        out16 = self.conv16(out15 + out2)
+        out17 = self.conv17(out16 + out2)
+        out18 = self.conv18(out17 + out2)
         return out18
